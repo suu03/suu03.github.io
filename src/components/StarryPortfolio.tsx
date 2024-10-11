a07c9de5c91d416253967bf16bb636b7dd87595c
suu03 <138574125+suu03@users.noreply.github.com>
2024-10-11 13:49:50 +0900

    add icon for language switch and links

--- src/components/StarryPortfolio.tsx
+++ src/components/StarryPortfolio.tsx
@@ -28,7 +28,6 @@ const GlowMaterial = shaderMaterial(
 
 extend({ GlowMaterial });
 
-// New HoverableText component
 function HoverableText({
   position,
   fontSize,
@@ -57,15 +56,15 @@ function HoverableText({
       position={position}
       fontSize={fontSize}
       color={hovered ? hoverColor : color}
-      anchorX={
-        typeof anchorX === "string"
-          ? (anchorX as "left" | "center" | "right")
-          : anchorX
-      }
+      anchorX={anchorX as "left" | "center" | "right"}
       anchorY={
-        typeof anchorY === "string"
-          ? (anchorY as "left" | "center" | "right")
-          : anchorY
+        anchorY as
+          | "top"
+          | "bottom"
+          | "top-baseline"
+          | "middle"
+          | "bottom-baseline"
+          | number
       }
       rotation={rotation}
       onPointerOver={() => setHover(true)}
@@ -82,6 +81,18 @@ function Box(
 ) {
   const meshRef = useRef<THREE.Mesh>(null!);
   const glowRef = useRef<THREE.Mesh>(null!);
+  const [scale, setScale] = useState(1);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobile = window.innerWidth < 768;
+      setScale(isMobile ? 0.7 : 1);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   useFrame((_, delta) => {
     meshRef.current.rotation.x += delta * 0.2;
@@ -95,10 +106,12 @@ function Box(
     []
   );
 
+  const boxSize = 4;
+
   return (
-    <group>
+    <group scale={[scale, scale, scale]}>
       <mesh {...props} ref={meshRef}>
-        <boxGeometry args={[4, 4, 4]} />
+        <boxGeometry args={[boxSize, boxSize, boxSize]} />
         {colors.map((color, index) => (
           <meshStandardMaterial
             key={index}
@@ -107,7 +120,7 @@ function Box(
           />
         ))}
         <HoverableText
-          position={[0, 0, 3]} // Front Face
+          position={[0, 0, boxSize / 2 + 0.5]}
           fontSize={0.7}
           color="#00BDCE"
           hoverColor="#00BDCE"
@@ -118,7 +131,7 @@ function Box(
           Suu portfolio
         </HoverableText>
         <HoverableText
-          position={[3, 0, 0]} // Right Face
+          position={[boxSize / 2 + 0.5, 0, 0]}
           fontSize={0.9}
           color="black"
           hoverColor="#0056EB"
@@ -130,7 +143,7 @@ function Box(
           Github
         </HoverableText>
         <HoverableText
-          position={[0, 3, 0]} // Top Face
+          position={[0, boxSize / 2 + 0.5, 0]}
           fontSize={0.9}
           color="black"
           hoverColor="#146C8D"
@@ -142,7 +155,7 @@ function Box(
           Contact
         </HoverableText>
         <HoverableText
-          position={[-3, 0, 0]} // Left Face
+          position={[-boxSize / 2 - 0.5, 0, 0]}
           fontSize={0.7}
           color="black"
           hoverColor="#3068A8"
@@ -154,7 +167,7 @@ function Box(
           Make a Wish!
         </HoverableText>
         <HoverableText
-          position={[0, -3, 0]}
+          position={[0, -boxSize / 2 - 0.5, 0]}
           fontSize={0.7}
           color="black"
           hoverColor="#3567BF"
@@ -166,20 +179,20 @@ function Box(
           Donate me!
         </HoverableText>
         <HoverableText
-          position={[0, 0, -3]} // Back Face
-          fontSize={0.9} // Changed from 0 to 0.8
+          position={[0, 0, -boxSize / 2 - 0.5]}
+          fontSize={0.9}
           color="black"
           hoverColor="#1F80FF"
           anchorX="center"
           anchorY="middle"
-          rotation={[0, Math.PI, 0]} // Adjusted rotation for better visibility
+          rotation={[0, Math.PI, 0]}
           onClick={() => props.setActiveSection("About")}
         >
           About
         </HoverableText>
       </mesh>
       <mesh ref={glowRef} scale={1.2}>
-        <boxGeometry args={[4, 4, 4]} />
+        <boxGeometry args={[boxSize, boxSize, boxSize]} />
       </mesh>
     </group>
   );
@@ -225,6 +238,7 @@ function Modal({
   setActiveSection: (section: string) => void;
 }) {
   const [isVisible, setIsVisible] = useState(false);
+  const [language, setLanguage] = useState<"en" | "jp">("en");
 
   useEffect(() => {
     if (activeSection) {
@@ -233,16 +247,43 @@ function Modal({
   }, [activeSection]);
 
   const content = {
-    About:
-      "こんにちは！私はSuuです。\n小さな島国から来ました。\n縁あって日本で生活し、2019年からフロントエンドエンジニアとして働き始めました。\n他の詳細は私のポートフォリオに隠されています。\n気に入っていただけると嬉しいです！",
-    Github: "Check out my latest projects,\nincluding this 3D portfolio!",
-    Contact: "Get in touch with me at\nexample@email.com",
-    Donate: "Donate me!",
+    About: {
+      en: "Hello! I'm Suu.\nI come from a small island country.\nBy chance, I started living in Japan and began working as a front-end engineer in 2019.\nOther details are hidden in my portfolio.\nI hope you like it!",
+      jp: "こんにちは！私はSuuです。\n小さな島国から来ました。\n縁あって日本で生活し、2019年からフロントエンドエンジニアとして働き始めました。\n他の詳細は私のポートフォリオに隠されています。\n気に入っていただけると嬉しいです！",
+    },
+    Github: {
+      en: "Check out my latest projects,\nincluding this 3D portfolio!",
+      jp: "この3Dポートフォリオを含む\n最新のプロジェクトをチェックしてください！",
+    },
+    Contact: {
+      en: "Get in touch with me at\nsuu0205p@gmail.com",
+      jp: "suu0205p@gmail.com\nでご連絡ください",
+    },
+    Donate: {
+      en: "Your support helps me continue creating and improving projects like this one.\nThank you for your generosity!",
+      jp: "あなたのサポートは、このようなプロジェクトの作成と改善を続けるのに役立ちます。\nご支援ありがとうございます！",
+    },
   };
 
   const handleClose = () => {
     setIsVisible(false);
-    setTimeout(() => setActiveSection(""), 300); // Delay to allow exit animation
+    setTimeout(() => setActiveSection(""), 300);
+  };
+
+  const toggleLanguage = () => {
+    setLanguage((prev) => (prev === "en" ? "jp" : "en"));
+  };
+
+  const handleSendEmail = () => {
+    window.location.href = "mailto:suu0205p@gmail.com";
+  };
+
+  const handleGithubLink = () => {
+    window.open("https://github.com/suu03", "_blank");
+  };
+
+  const handleDonation = () => {
+    window.open("https://example.com/donate", "_blank");
   };
 
   return (
@@ -262,12 +303,66 @@ function Modal({
             transition={{ delay: 0.3, duration: 0.5 }}
             className="bg-white bg-opacity-80 backdrop-blur-md w-full max-w-2xl p-8 rounded-lg shadow-2xl"
           >
-            <h2 className="text-4xl font-bold mb-6 text-cyan-500 border-b pb-2">
-              {activeSection}
-            </h2>
+            <div className="flex justify-between items-center mb-6">
+              <h2 className="text-4xl font-bold text-cyan-500">
+                {activeSection}
+              </h2>
+              <button
+                onClick={toggleLanguage}
+                className="w-10 h-10 focus:outline-none"
+              >
+                <img
+                  src={
+                    language === "en"
+                      ? "/japanese_icon.svg"
+                      : "/english_icon.svg"
+                  }
+                  alt={
+                    language === "en"
+                      ? "Switch to Japanese"
+                      : "Switch to English"
+                  }
+                  className="w-full h-full"
+                />
+              </button>
+            </div>
             <p className="whitespace-pre-line text-lg text-slate-700 mb-6">
-              {content[activeSection as keyof typeof content]}
+              {content[activeSection as keyof typeof content][language]}
             </p>
+            {activeSection === "Contact" && (
+              <div className="flex justify-center mb-6">
+                <button
+                  onClick={handleSendEmail}
+                  className="focus:outline-none opacity-70 hover:opacity-100 transition-opacity duration-300"
+                >
+                  <img src="/send.svg" alt="Send Email" className="w-12 h-12" />
+                </button>
+              </div>
+            )}
+            {activeSection === "Github" && (
+              <div className="flex justify-center mb-6">
+                <button
+                  onClick={handleGithubLink}
+                  className="focus:outline-none opacity-70 hover:opacity-100 transition-opacity duration-300"
+                >
+                  <img
+                    src="/github.svg"
+                    alt="Visit GitHub"
+                    className="w-12 h-12"
+                  />
+                </button>
+              </div>
+            )}
+            {activeSection === "Donate" && (
+              <div className="flex justify-center mb-6">
+                <button
+                  onClick={handleDonation}
+                  className="focus:outline-none opacity-70 hover:opacity-100 transition-opacity duration-300"
+                >
+                  <img src="/donate.svg" alt="Donate" className="w-12 h-12" />
+                </button>
+              </div>
+            )}
             <div className="flex justify-end">
               <button
                 className="px-4 py-2 text-slate-800 hover:text-cyan-500 transition-colors duration-300"
